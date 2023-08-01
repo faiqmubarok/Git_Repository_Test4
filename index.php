62011d7515d52572c36ff7787a3713505ea5bda8
faiq <faiqmubarok@gmail.com>
2023-08-02 00:07:43 +0700

    Menambah file index.php

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur veritatis est accusamus expedita facilis aperiam ratione voluptatum ea ab corrupti, minima odit, quis, voluptatem velit odio neque inventore culpa reprehenderit.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
